b0d99dc0cf755e6d99316784de5d603788bcc228
PrakashRajendran <prprakash2007@gmail.com>
2015-03-25 23:11:38 -0400

    updated files

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
-	//'url' : 'mongodb://localhost/socketwebapp'
+	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
+	'url' : 'mongodb://socketwebadmin:socketwebadmin@ds061188.mongolab.com:61188/socketwebapp'
 }
