20a1bc8c978cd5c0fc8d447de81d4e84eaf8ef59
Prakash Rajendran <200263093@BA-B215-36.student.georgianc.on.ca>
2015-03-26 15:26:14 -0400

    updated new files

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 /* Define the database uri string */
 module.exports = {
-	'url' : 'mongodb://localhost/socketwebapp' 
-    //'url' : 'mongodb://socketwebadmin:socketwebadmin@ds061188.mongolab.com:61188/socketwebapp'
+	//'url' : 'mongodb://localhost/socketwebapp' 
+    'url' : 'mongodb://socketwebadmin:socketwebadmin@ds061188.mongolab.com:61188/socketwebapp'
 }
